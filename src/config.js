ed57c93cb40873deba5ee913057d438422f94bc0
Ashton <ammendoza2@myseneca.ca>
2025-12-10 02:19:33 -0500

    Update API URL to use ECS load balancer for Lab 9

--- src/config.js
+++ src/config.js
@@ -1,7 +1,7 @@
 // src/config.js
 
 export const config = {
-  apiUrl: process.env.API_URL || 'http://54.235.18.43:8080',
+  apiUrl: process.env.API_URL || 'http://fragments-lb-1601750164.us-east-1.elb.amazonaws.com',
   cognito: {
     poolId: process.env.AWS_COGNITO_POOL_ID || 'us-east-1_09DPjH0Gn',
     clientId: process.env.AWS_COGNITO_CLIENT_ID || '2gr73fhi7aogfoobcb8cjkrsgm',
